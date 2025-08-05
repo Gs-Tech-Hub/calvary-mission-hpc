8da842ad3e381c9aadfc06e1f51d6df75bc982ca
Prnzdiamond <oseahumenagboifoh@gmail.com>
2025-08-05 22:07:11 +0100

    chore: create project folder structure and init modules - closes #1

---
+++ frontend/admin-panel/src/app/index/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+    return (
+        <main className="p-4">
+            <h1 className="text-2xl font-bold">testing from ShadCN UI</h1>
+            <Button variant="default">Click me</Button>
+        </main>
+    );
+}
